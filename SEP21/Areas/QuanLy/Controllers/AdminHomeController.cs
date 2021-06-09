814de5e176e09461205e74c1d48f1cc6619ed49b
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2021-06-10 01:04:51 +0700

    Add Admin template

---
+++ SEP21/Areas/QuanLy/Controllers/AdminHomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEP21.Areas.QuanLy.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: QuanLy/AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: QuanLy/AdminHome/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: QuanLy/AdminHome/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QuanLy/AdminHome/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QuanLy/AdminHome/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: QuanLy/AdminHome/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QuanLy/AdminHome/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: QuanLy/AdminHome/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
