301df0402e70da770ff85abc22a52eaeb347070a
187PM06734 - TRAN LE ANH TRUNG - T5 <trung.187pm06734@vanlanguni.vn>
2021-07-05 15:21:35 +0700

    IndexTuyenDung

---
+++ SEP21/Controllers/TuyenDungController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEP21.Models;
+
+namespace SEP21.Controllers
+{
+    public class TuyenDungController : Controller
+    {
+        private SEP24Team5Entities db = new SEP24Team5Entities();
+        // GET: TuyenDung
+        public ActionResult Index()
+        {
+            var Tuyendung1 = db.TuyenDungs.Include(a => a.LoaiTuyenDung);
+            return View(Tuyendung1.ToList());
+        }
+    }
+}
