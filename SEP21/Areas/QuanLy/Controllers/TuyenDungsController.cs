a7cb1ae6f6d300ab9082010778a4248ce2d072a3
187PM06734 - TRAN LE ANH TRUNG - T5 <trung.187pm06734@vanlanguni.vn>
2021-07-08 23:52:42 +0700

    Chỉnh sửa admin

---
+++ SEP21/Areas/QuanLy/Controllers/TuyenDungsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEP21.Models;
+
+namespace SEP21.Areas.QuanLy.Controllers
+{
+    public class TuyenDungsController : Controller
+    {
+        private SEP24Team5Entities db = new SEP24Team5Entities();
+
+        // GET: QuanLy/TuyenDungs
+        public ActionResult Index()
+        {
+            var tuyenDungs = db.TuyenDungs.Include(t => t.LoaiTuyenDung1);
+            return View(tuyenDungs.ToList());
+        }
+
+        // GET: QuanLy/TuyenDungs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TuyenDung tuyenDung = db.TuyenDungs.Find(id);
+            if (tuyenDung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuyenDung);
+        }
+
+        // GET: QuanLy/TuyenDungs/Create
+        public ActionResult Create()
+        {
+            ViewBag.LoaiTuyenDung = new SelectList(db.LoaiTuyenDungs, "ID", "LoaiTuyenDung1");
+            return View();
+        }
+
+        // POST: QuanLy/TuyenDungs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ViTri,SoLuong,YeuCau,TieuDe,LoaiTuyenDung")] TuyenDung tuyenDung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TuyenDungs.Add(tuyenDung);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LoaiTuyenDung = new SelectList(db.LoaiTuyenDungs, "ID", "LoaiTuyenDung1", tuyenDung.LoaiTuyenDung);
+            return View(tuyenDung);
+        }
+
+        // GET: QuanLy/TuyenDungs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TuyenDung tuyenDung = db.TuyenDungs.Find(id);
+            if (tuyenDung == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LoaiTuyenDung = new SelectList(db.LoaiTuyenDungs, "ID", "LoaiTuyenDung1", tuyenDung.LoaiTuyenDung);
+            return View(tuyenDung);
+        }
+
+        // POST: QuanLy/TuyenDungs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ViTri,SoLuong,YeuCau,TieuDe,LoaiTuyenDung")] TuyenDung tuyenDung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tuyenDung).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LoaiTuyenDung = new SelectList(db.LoaiTuyenDungs, "ID", "LoaiTuyenDung1", tuyenDung.LoaiTuyenDung);
+            return View(tuyenDung);
+        }
+
+        // GET: QuanLy/TuyenDungs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TuyenDung tuyenDung = db.TuyenDungs.Find(id);
+            if (tuyenDung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuyenDung);
+        }
+
+        // POST: QuanLy/TuyenDungs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TuyenDung tuyenDung = db.TuyenDungs.Find(id);
+            db.TuyenDungs.Remove(tuyenDung);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
