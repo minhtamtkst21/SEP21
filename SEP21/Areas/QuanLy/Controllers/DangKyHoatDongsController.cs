a0617e959095b2a657d4943736c950c0cef703e1
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2021-07-17 21:10:54 +0700

    đăng ký hoạt động

---
+++ SEP21/Areas/QuanLy/Controllers/DangKyHoatDongsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEP21.Models;
+
+namespace SEP21.Areas.QuanLy.Controllers
+{
+    public class DangKyHoatDongsController : Controller
+    {
+        private SEP24Team5Entities db = new SEP24Team5Entities();
+
+        // GET: QuanLy/DangKyHoatDongs
+        public ActionResult Index()
+        {
+            var dangKyHoatDongs = db.DangKyHoatDongs.Include(d => d.HoatDong1).Include(d => d.SinhVien);
+            return View(dangKyHoatDongs.ToList());
+        }
+
+        // GET: QuanLy/DangKyHoatDongs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DangKyHoatDong dangKyHoatDong = db.DangKyHoatDongs.Find(id);
+            if (dangKyHoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dangKyHoatDong);
+        }
+
+        // GET: QuanLy/DangKyHoatDongs/Create
+        public ActionResult Create()
+        {
+            ViewBag.HoatDong = new SelectList(db.HoatDongs, "ID", "TenHoatDong");
+            ViewBag.MSSV = new SelectList(db.SinhViens, "ID", "MSSV");
+            return View();
+        }
+
+        // POST: QuanLy/DangKyHoatDongs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,HoatDong,MSSV,ThoiGianDangKy")] DangKyHoatDong dangKyHoatDong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DangKyHoatDongs.Add(dangKyHoatDong);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HoatDong = new SelectList(db.HoatDongs, "ID", "TenHoatDong", dangKyHoatDong.HoatDong);
+            ViewBag.MSSV = new SelectList(db.SinhViens, "ID", "MSSV", dangKyHoatDong.MSSV);
+            return View(dangKyHoatDong);
+        }
+
+        // GET: QuanLy/DangKyHoatDongs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DangKyHoatDong dangKyHoatDong = db.DangKyHoatDongs.Find(id);
+            if (dangKyHoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HoatDong = new SelectList(db.HoatDongs, "ID", "TenHoatDong", dangKyHoatDong.HoatDong);
+            ViewBag.MSSV = new SelectList(db.SinhViens, "ID", "MSSV", dangKyHoatDong.MSSV);
+            return View(dangKyHoatDong);
+        }
+
+        // POST: QuanLy/DangKyHoatDongs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,HoatDong,MSSV,ThoiGianDangKy")] DangKyHoatDong dangKyHoatDong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dangKyHoatDong).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HoatDong = new SelectList(db.HoatDongs, "ID", "TenHoatDong", dangKyHoatDong.HoatDong);
+            ViewBag.MSSV = new SelectList(db.SinhViens, "ID", "MSSV", dangKyHoatDong.MSSV);
+            return View(dangKyHoatDong);
+        }
+
+        // GET: QuanLy/DangKyHoatDongs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DangKyHoatDong dangKyHoatDong = db.DangKyHoatDongs.Find(id);
+            if (dangKyHoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dangKyHoatDong);
+        }
+
+        // POST: QuanLy/DangKyHoatDongs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DangKyHoatDong dangKyHoatDong = db.DangKyHoatDongs.Find(id);
+            db.DangKyHoatDongs.Remove(dangKyHoatDong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
