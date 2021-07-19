5c6ee41f16150ec55e6c67a009baadcdf33f11a4
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2021-07-19 16:10:15 +0700

    fix DaoTao

--- SEP21/Areas/QuanLy/Controllers/DaoTaosController.cs
+++ SEP21/Areas/QuanLy/Controllers/DaoTaosController.cs
@@ -44,7 +44,7 @@ namespace SEP21.Areas.QuanLy.Controllers
         // POST: QuanLy/DaoTaos/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,NoiDung,TieuDe")] DaoTao daoTao)
         {
@@ -76,7 +76,7 @@ namespace SEP21.Areas.QuanLy.Controllers
         // POST: QuanLy/DaoTaos/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,NoiDung,TieuDe")] DaoTao daoTao)
         {
